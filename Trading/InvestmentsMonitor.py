c107cc6860ef2d52871188fc8762f190b737e9ef
Cristian Lupascu <cristian.lupascu@jlg.ro>
2015-05-29 12:28:44 +0300

    am inbunatatit monitorul

--- Trading/InvestmentsMonitor.py
+++ Trading/InvestmentsMonitor.py
@@ -9,24 +9,45 @@ class InvestmentMonitor:
     def __init__(self, debugflag, portofolio):
         self.portofolio = portofolio
         self.parser = ValueParser(debugflag)
+        self.previousValues = []
+        self.repeats = 0
 
     def monitor(self):
         print("Monitoring")
+        self.initializePreviousVectorValues()
+
         while True:
-            end = input("stop to end monitoring: ")
-            if "stop" in end:
-                break
+            if self.repeats == 0:
+                end = input("stop or number of repeats: ")
+                if "stop" in end:
+                    break
+                if end.isnumeric():
+                    self.repeats = int(end)
+
             myThread = threading.Thread(target=self.getCurrentValuesForInvestments(), args=())
             myThread.start()
             sleep(10)
+            self.repeats -= 1
         print("Monitoring is stoped")
 
 
     def getCurrentValuesForInvestments(self):
-        for investment in self.portofolio.investments:
+
+        for index in range(len(self.portofolio.investments)):
+            investment = self.portofolio.investments[index]
             if investment.open:
-                index = investment.onlineindex
-                currentValue = self.parser.getOnlineValueForCurrencyIndex(index)
+                currentValue = self.parser.getOnlineValueForCurrencyIndex(investment.onlineindex)
+
+                previousValue = self.previousValues[index]
+                self.previousValues[index] = currentValue
+
+                if previousValue > 0:
+                    if currentValue > previousValue:
+                        mediumStatus = "RISING"
+                    elif currentValue < previousValue:
+                        mediumStatus = "LOWERING"
+                    else: mediumStatus = "STEADY"
+                else: mediumStatus = "--"
 
                 if currentValue > investment.initialPrice:
                     status = "UP"
@@ -35,5 +56,9 @@ class InvestmentMonitor:
                 else: status = "SAME"
 
                 print(investment)
-                print("Current value = " + str(currentValue) + ", " + status)
+                print("Current value = {}, {}, {}".format(currentValue, status, mediumStatus))
 
+
+    def initializePreviousVectorValues(self):
+        for index in range(len(self.portofolio.investments)):
+            self.previousValues.append(0)
